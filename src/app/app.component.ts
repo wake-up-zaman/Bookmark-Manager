d3485aab22794b7b52f08610d734ee9214378685
wakeupzaman909 <hadiuzzaman909@outlook.com>
2023-08-03 12:58:51 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bookmark-manager';
+  isMenuOpen = false;
+  activeTab: string = 'panel-1'; 
+
+  selectTab(tab: string): void {
+    this.activeTab = tab; 
+  }
+
+  navToggle() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+  
+
 }
+
